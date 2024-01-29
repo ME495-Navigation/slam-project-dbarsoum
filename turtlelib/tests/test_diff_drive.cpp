63c2b4a7f76aa5972f466771e861bf359d009aa2
dbarsoum <demianabarsoum2027@u.northwestern.edu>
2024-01-28 20:26:01 -0600

    added target_link_libraries to nuturtle_control cmake to build correctly

--- turtlelib/tests/test_diff_drive.cpp
+++ turtlelib/tests/test_diff_drive.cpp
@@ -223,3 +223,34 @@ TEST_CASE("Test DiffDrive", "[DiffDrive_backward_leftturn]")
     REQUIRE_THAT(wheel_velocities.phi_left, Catch::Matchers::WithinAbs(new_wheel_positions.phi_left, 1e-12));
     REQUIRE_THAT(wheel_velocities.phi_right, Catch::Matchers::WithinAbs(new_wheel_positions.phi_right, 1e-12));
 }
+
+// TEST_CASE("Test DiffDrive", "[DiffDrive_no_twist]")
+// {
+//     double wheel_track = 1.0;
+//     double wheel_radius = 2.0;
+//     DiffDrive diff_drive(wheel_track, wheel_radius);
+//     WheelPositions_phi new_wheel_positions;
+//     Configuration_q config;
+
+//     new_wheel_positions.phi_left = 0.0;
+//     new_wheel_positions.phi_right = 0.0;
+//     diff_drive.update_configuration(new_wheel_positions);
+
+//     config.theta_= 0.0;
+//     config.x_ = 0.0;
+//     config.y_ = 0.0;
+
+//     REQUIRE_THAT(diff_drive.get_configuration().theta_, Catch::Matchers::WithinAbs(config.theta_, 1e-12));
+//     REQUIRE_THAT(diff_drive.get_configuration().x_, Catch::Matchers::WithinAbs(config.x_, 1e-12));
+//     REQUIRE_THAT(diff_drive.get_configuration().y_, Catch::Matchers::WithinAbs(config.y_, 1e-12));
+
+//     diff_drive = DiffDrive(wheel_track, wheel_radius);
+//     Twist2D twist;
+//     twist.omega = 0.0;
+//     twist.x = 0.0;
+//     twist.y = 2.0;
+
+//     WheelPositions_phi wheel_velocities = diff_drive.compute_wheel_velocities(twist);
+//     /// make sure twist cannot be accomplished with current configuration
+//     REQUIRE_THAT(wheel_velocities.phi_left, Catch::Matchers::WithinAbs(0.0, 1e-12));
+// }
